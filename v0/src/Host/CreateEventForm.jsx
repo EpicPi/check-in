948e0fbccf2b2a13307afca1acb783092cf898ae
Sam Baek <kie827@gmail.com>
2018-02-10 16:37:31 -0500

    basic host dash

---
+++ v0/src/Host/CreateEventForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class CreateEventForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventName: '',
+      code: '',
+    };
+
+    this.handleNameInput = this.handleNameInput.bind(this);
+    this.handleCodeInput = this.handleCodeInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameInput(e) {
+    this.setState({ eventName: e.target.value });
+  }
+
+  handleCodeInput(e) {
+    this.setState({ code: e.target.value });
+  }
+
+  handleSubmit(e) {
+      alert(this.state.eventName + " " + this.state.code);
+      e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form action="">
+        <label>
+                Event Name:
+                <input type="text" name="eventName" value={this.state.eventName} onChange={this.handleNameInput} />
+        </label>
+        <label>
+                Code:
+                <input type="text" name="code" value={this.state.code} onChange={this.handleCodeInput}/>
+        </label>
+        <button type="submit" value="Submit" onClick={this.handleSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default CreateEventForm;
--- v0/src/Host/HostDash.jsx
+++ v0/src/Host/HostDash.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import CreateEventForm from './CreateEventForm';
+
 
 class HostDash extends Component {
 
+
   render() {
     return (
-      <h1>Host { this.props.name }!</h1>
+      <div>
+        <h1>Host { this.props.name }!</h1>
+        <Switch>
+          <Route exact path="/" component="HostHome" />
+          <Route path="/create" component={ CreateEventForm } />
+          <Route path="/view" component={ ViewEvents }/>
+        </Switch>
+        <button>create an event</button>
+        <button>view my events</button>
+      </div>
     );
   }
 }
