77250c3f70843a317419b904ab894186b6c46c4f
Sam Baek <kie827@gmail.com>
2018-02-10 17:08:34 -0500

    create event basic

--- v0/src/Host/HostDash.jsx
+++ v0/src/Host/HostDash.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
 
+import HostHome from './HostHome';
 import CreateEventForm from './CreateEventForm';
-
+import ViewEvents from './ViewEvents';
 
 class HostDash extends Component {
-
-
   render() {
     return (
       <div>
         <h1>Host { this.props.name }!</h1>
-        <Switch>
-          <Route exact path="/" component="HostHome" />
-          <Route path="/create" component={ CreateEventForm } />
-          <Route path="/view" component={ ViewEvents }/>
-        </Switch>
-        <button>create an event</button>
-        <button>view my events</button>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HostHome} />
+            <Route path="/create" component={CreateEventForm} />
+            <Route path="/view" component={ViewEvents} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ v0/src/Host/ViewEvents.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ViewEvents extends Component {
+  render() {
+    return (
+      <div>
+        Viewing list!
+      </div>
+    );
+  }
+}
+
+export default ViewEvents;
