afccba8254425c125d974675e9cfc502a8f2ed5a
EpicPi <piyushgk1@gmail.com>
2018-02-10 15:45:54 -0500

    testing the code for cookies

--- v0/src/App.jsx
+++ v0/src/App.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GuestDash from './Guest/GuestDash';
 import HostDash from './Host/HostDash';
+import Cookies from 'universal-cookie';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.cookies = new Cookies();
   }
 
   render() {
+      this.cookies.set('name', 'Bobby', { path: '/' });
     return (
       <div>
         <BrowserRouter>
--- v0/src/Guest/GuestDash.jsx
+++ v0/src/Guest/GuestDash.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
 
 class GuestDash extends Component {
+  constructor(props){
+    super(props);
+      this.cookies = new Cookies();
+  }
   render() {
+    console.log(this.cookies.get('name'));
     return (
-      <h1>Guest Hello!</h1>
+      <h1>{this.cookies.get('name')}</h1>
     );
   }
 }
