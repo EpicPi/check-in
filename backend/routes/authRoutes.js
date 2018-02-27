87e3e681547e0b68e077b60b32204a35ac7ed297
EpicPi <piyushgk1@gmail.com>
2018-02-26 20:34:53 -0500

    authRoutes redict correctly

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -10,7 +10,7 @@ module.exports = (router) => {
     router.use('/auth/google/callback',
         passport.authenticate('google'),
         (req, res) => {
-            res.redirect('/');
+            res.redirect('/dash');
         }
     );
 
