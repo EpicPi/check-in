876bd3bd90cc25e41bac12aa529a9069c55e7b67
Sam Baek <kie827@gmail.com>
2018-03-03 14:28:43 -0500

    add date to event.js model

--- backend/models/event.js
+++ backend/models/event.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const eventSchema = new Schema({
-    name: String,
-    code: String,
+    name: {type: String, required: true},
+    code: {type: String, required: true},
     guestsRSVP: [],
     guestsAttend: [],
+    rsvpStart: {type: Date, required: true},
+    rsvpEnd: {type: Date, required: true},
+    checkinStart: {type: Date, required: true},
+    checkinEnd: {type: Date, required: true},
 });
 
 mongoose.model('events',eventSchema);
