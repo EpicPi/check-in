579b32d94f9e93c77394b402eb0a29c5b50806a0
EpicPi <piyushgk1@gmail.com>
2018-04-02 00:47:26 -0400

    add actions and backend routes for group

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -25,4 +25,44 @@ router.post('/check', async (req, res) => {
   res.send(group);
 });
 
+router.post('/add_group', async (req, res) => {
+  const user = await User.findById(req.user.id);
+  const group = await Group({
+    name: req.body.name,
+    code: req.body.code,
+    users: [req.user.id],
+    events: []
+  }).save();
+  user.hostGroups.push(group.id);
+  user.save();
+});
+
+router.post('edit_group', async (req, res) => {
+  const group = await Group.findById(req.body.id);
+  if (group) {
+    group.name = req.body.name;
+    group.code = req.body.code;
+  } else
+    console.error(
+      '[ERR] Group was not found. Passed in code: ' +
+        req.body.code +
+        ' in /group/edit_group'
+    );
+});
+
+router.get('/get_groups', async (req, res) => {
+  const user = await User.findById(req.user.id);
+  const out = user.hostGroups.map(id => Group.findById(id));
+  res.send(Promise.all(out));
+});
+
+router.post('/remove_group', async (req, res) => {
+  const group = Group.findById(req.body.id);
+  let users = group.users.forEach(async id => {
+    const user = await User.findById(id);
+    user.hostGroups.filter(el => el !== req.body.id);
+  });
+  group.remove();
+});
+
 module.exports = router;
--- frontend/actions/groupActions.js
+++ frontend/actions/groupActions.js
@@ -15,3 +15,19 @@ export const getGroups = () => async dispatch => {
   const res = await axios.get(groupRoute + 'get_groups');
   dispatch({ type: GOT_GROUPS, payload: res.data });
 };
+
+export const removeGroup = group => dispatch => {
+  dispatch({ type: REMOVE_GROUP, payload: group });
+  axios.post(groupRoute + 'remove_group', qs.stringify({ id: group.id }));
+};
+
+export const joinGroup = group => async dispatch => {
+  dispatch({ type: ADD_GROUP, payload: group });
+  await axios.post(groupRoute + 'join', qs.stringify({ id: group.id }));
+};
+
+export const checkGroupCode = code => async dispatch => {
+  dispatch({ type: CHECK_GROUP_CODE });
+  const res = axios.post(groupRoute + 'check', qs.stringify({ code: code }));
+  dispatch({ type: CHECKED_GROUP_CODE, payload: res });
+};
