f7628ecd01899cb9badd691257ea52e15d753294
EpicPi <piyushgk1@gmail.com>
2018-04-11 23:45:02 -0400

    show events

--- frontend/components/Group/Show/GroupDetail.js
+++ frontend/components/Group/Show/GroupDetail.js
@@ -2,13 +2,168 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { getGroupEvents } from '../../../actions/groupActions';
 import { selectEvent } from '../../../actions';
+import { LOAD } from '../../../helpers/Enums';
+import { isEventActive, isEventClosed } from '../../../helpers/Time';
+import EventItem from '../../Host/Show/EventItem';
 
 class GroupDetail extends Component {
   constructor(props) {
     super(props);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+
+    if (this.props.events === LOAD.NOTHING) this.props.getEvents();
+    this.state = {
+      out: this.getEventsOutput(this.props),
+      active: this.getActiveEventsOutput(this.props),
+      closed: this.getClosedEventsOutput(this.props)
+    };
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      out: this.getEventsOutput(nextProps),
+      active: this.getActiveEventsOutput(nextProps),
+      closed: this.getClosedEventsOutput(nextProps)
+    });
+  }
+
+  handleCreate() {
+    this.props.history.push('/host/create');
+  }
+
+  handleEdit() {
+    this.props.history.push('/group/edit');
+  }
+
+  getEventsOutput(props) {
+    switch (props.events) {
+      case LOAD.LOADING:
+        return <h3>LOADING</h3>;
+      case LOAD.NOTHING:
+        return;
+      default:
+        if (
+          props.events.filter(
+            event => !isEventActive(event) && !isEventClosed(event)
+          ).length > 0
+        )
+          return (
+            <div>
+              <h3>Open</h3>
+              <div className="row">
+                <div className="col-md-12">
+                  <ul className="event-list">
+                    {props.events
+                      .filter(
+                        event => !isEventActive(event) && !isEventClosed(event)
+                      )
+                      .map((event, i) => (
+                        <EventItem
+                          history={props.history}
+                          key={i}
+                          event={event}
+                          group={props.group._id}
+                        />
+                      ))}
+                  </ul>
+                </div>
+              </div>
+              <hr />
+            </div>
+          );
+    }
+  }
+
+  getActiveEventsOutput(props) {
+    switch (props.events) {
+      case LOAD.LOADING:
+        return <h3>LOADING</h3>;
+      case LOAD.NOTHING:
+        return;
+      default:
+        if (props.events.filter(event => isEventActive(event)).length > 0)
+          return (
+            <div>
+              <h3>CheckIn Active</h3>
+              <div className="row">
+                <div className="col-md-12">
+                  <ul className="event-list">
+                    {props.events
+                      .filter(event => isEventActive(event))
+                      .map((event, i) => (
+                        <EventItem
+                          history={props.history}
+                          key={i}
+                          event={event}
+                          group={props.group._id}
+                        />
+                      ))}
+                  </ul>
+                </div>
+              </div>
+              <hr />
+            </div>
+          );
+    }
+  }
+
+  getClosedEventsOutput(props) {
+    switch (props.events) {
+      case LOAD.LOADING:
+        return <h3>LOADING</h3>;
+      case LOAD.NOTHING:
+        return;
+      default:
+        if (props.events.filter(event => isEventClosed(event)).length > 0)
+          return (
+            <div>
+              <h3>Closed</h3>
+              <div className="row">
+                <div className="col-md-12">
+                  <ul className="event-list">
+                    {props.events
+                      .filter(event => isEventClosed(event))
+                      .map((event, i) => (
+                        <EventItem
+                          history={props.history}
+                          key={i}
+                          event={event}
+                          group={props.group._id}
+                        />
+                      ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+    }
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="row host-show">
+        <div className="container-fluid">
+          <div className="row btn-create">
+            <div className="col-md-12">
+              <button
+                className="btn btn-lg btn-info"
+                onClick={this.handleCreate}
+              >
+                Create Event
+              </button>
+              <button className="btn btn-lg btn-info" onClick={this.handleEdit}>
+                Edit
+              </button>
+            </div>
+          </div>
+          <hr />
+          {this.state.active}
+          {this.state.out}
+          {this.state.closed}
+        </div>
+      </div>
+    );
   }
 }
 
@@ -21,8 +176,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (/* dispatch */) => {
   return {
-    getEvents: getGroupEvents,
-    selectEvent: selectEvent
+    selectEvent: selectEvent,
+    getEvents: getGroupEvents
   };
 };
 
