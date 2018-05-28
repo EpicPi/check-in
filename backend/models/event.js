093a13f7928752e63a8c65cd2449e403e04ec367
Sammy Baek <kie827@gmail.com>
2018-05-28 16:18:54 -0400

    remove repeats on backend

--- backend/models/event.js
+++ backend/models/event.js
@@ -22,26 +22,7 @@ const eventSchema = new Schema({
   },
   type: { type: String, required: true },
   checkinCode: { type: String },
-  info: { type: String },
-  repeats: {
-    type: {
-      monday: { type: Boolean, default: false },
-      tuesday: { type: Boolean, default: false },
-      wednesday: { type: Boolean, default: false },
-      thursday: { type: Boolean, default: false },
-      friday: { type: Boolean, default: false },
-      saturday: { type: Boolean, default: false },
-      sunday: { type: Boolean, default: false }
-    }
-  }
+  info: { type: String }
 });
-
-eventSchema.methods.isRepeat = async function() {
-  let isRepeat = false;
-  for (let prop in this.repeats) {
-    isRepeat = !!(this.repeats[prop] || isRepeat);
-  }
-  return isRepeat;
-};
 
 mongoose.model('events', eventSchema);
